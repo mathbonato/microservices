0b330ce1132764f7e48ff6814fdb3ccefb46ebcf
Matheus Bonato <matheusmbonato@gmail.com>
2022-04-20 00:32:03 -0300

    adding Swagger API documentation

--- yourbank/src/server.ts
+++ yourbank/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { routes } from './infra/config/routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../swagger.json';
 
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 
 app.listen("3001", () => console.log("Server is running!"));
