19f95fa8d49039ea53108c001bdd22d9bc9f2050
neresdu <eduardoneress12@gmail.com>
2022-04-26 21:20:50 -0300

    Configuring swagger

--- sendEmail/src/main/java/com/SpringEmailAPI/SpringEmailAPI/SpringEmailApiApplication.java
+++ sendEmail/src/main/java/com/SpringEmailAPI/SpringEmailAPI/SpringEmailApiApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import com.SpringEmailAPI.SpringEmailAPI.service.EmailSenderService;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringEmailApiApplication {
 
 	public static void main(String[] args) {
--- sendEmail/src/main/java/com/SpringEmailAPI/SpringEmailAPI/controller/EmailController.java
+++ sendEmail/src/main/java/com/SpringEmailAPI/SpringEmailAPI/controller/EmailController.java
@@ -1,18 +1,29 @@
 package com.SpringEmailAPI.SpringEmailAPI.controller;
 
 import com.SpringEmailAPI.SpringEmailAPI.service.EmailSenderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.SpringEmailAPI.SpringEmailAPI.model.Email;
 
 @RestController
 @RequestMapping("/email")
+//@Api(value="API REST Emails")
+//@CrossOrigin(origins = "*")
 public class EmailController {
     @Autowired
     private EmailSenderService emailSenderService;
     @PostMapping(path = "/enviar")
+//    @ApiOperation(value="Enviar email")
     public void enviarEmail(@RequestBody Email email) {
+
         emailSenderService.sendEmail(email);
     }
+
+    @GetMapping("/hello")
+        public String hello(){
+            return "Hello World";
+        }
 
 }
