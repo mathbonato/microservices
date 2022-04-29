e79f7f1f761eeeea6ba8d19bdf7ce02acff31a41
Matheus Bonato <matheusmbonato@gmail.com>
2022-04-28 21:04:41 -0300

    adding fix on viaCep

--- yourbank/src/infra/service/viaCep.ts
+++ yourbank/src/infra/service/viaCep.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export class viaCep{
-    public getAddress (zipCode: string ): void{
-        axios.get(`viacep.com.br/ws/${zipCode}/json`).then((res)=>console.log(res.data)).catch(console.error)
+    public getAddress (zipCode: string ): void {
+        axios.get(`http://www.viacep.com.br/ws/${zipCode}/json`).then((res)=>console.log(res.data)).catch(console.error)
     }
 }
--- yourbank/src/server.ts
+++ yourbank/src/server.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 
-app.listen("3001", () => console.log("Server is running!"));
+app.listen("3002", () => console.log("Server is running!"));
